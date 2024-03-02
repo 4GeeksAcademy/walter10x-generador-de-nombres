8755d04ff184c311885ba67f2dff174017eeebce
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-03-02 02:14:54 +0000

    Primer avance de generador de dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  const generarNombres = () => {
+    let nombredominios = [];
+    // Iterar sobre el array pronoun que a su vez itera sobre adj y este a su vex itera sobre noum
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          // Agregar el nombre de dominio al array.
+          let nombredominio = pronoun[i] + adj[a] + noun[n] + ".com";
+          nombredominios.push(nombredominio);
+        }
+      }
+    }
+    // Retornar el array con todas las combinaciones posibles
+    return nombredominios;
+  };
+
+  let nombredominios = generarNombres(pronoun, adj, noun);
+
+  // Imprimir los nombres de dominio correctamente
+  for (let x = 0; x < nombredominios.length; x++) {
+    console.log(nombredominios[x]);
+  }
 };
